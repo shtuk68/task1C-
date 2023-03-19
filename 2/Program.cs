699b902d70c7f0efea46bda1311d4ab3680fc7ef
nikita <shtukolimp@mail.ru>
2023-03-19 12:52:15 +0300

    никитос ввёл имя

---
+++ 2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("введите имя ");
+String username = Console.ReadLine();
+Console.Write("привет, ");
+Console.Write(username);
